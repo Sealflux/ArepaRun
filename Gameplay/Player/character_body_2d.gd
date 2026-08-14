5331db1660666d27e9973df6ada9d6936da2d524
Thuan Nguyen <thuannguyen5177@gmail.com>
2026-08-14 18:36:25 -0500

    Player basic script

---
+++ Gameplay/Player/character_body_2d.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var SPEED = 400
+@export var JUMP_VELOCITY = -400.0
+var screen_size
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var direction := Input.get_axis("move_left","move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
