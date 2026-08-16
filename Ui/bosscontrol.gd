8c1b22b9778588debe65b1c3408d3fc52b60ab02
Thuan Nguyen <thuannguyen5177@gmail.com>
2026-08-16 10:53:32 -0500

    Began implementing boss

---
+++ Ui/bosscontrol.gd
@@ -0,0 +1,28 @@
+extends Control
+
+const MAX_HEALTH = 14
+var health = MAX_HEALTH
+
+func _ready() -> void:
+	update_health_ui()
+	$HealthBar.max_value = MAX_HEALTH
+
+func update_health_ui():
+	set_health_label()
+	set_health_bar()
+
+func set_health_label() -> void: 
+	$HealthLabel.text = "Health: %s" % health
+
+func set_health_bar() -> void:
+	$HealthBar.value = health
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		damage()
+
+func damage() -> void:
+	health -= 1
+	if health < 0:
+		health = MAX_HEALTH
+		update_health_ui()
